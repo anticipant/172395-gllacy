6386b508f9514b90a3bbf742136bf778836f7a80
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2016-05-19 20:18:05 +0300

    финальная сдача проекта ( без минимизации)

--- js/form.js
+++ js/form.js
@@ -1,30 +1,34 @@
     var link = document.querySelector(".js-feedback");
-    var popup = document.querySelector(".feedback-bg");
-    var popup2 = document.querySelector(".feedback");  
+    var popup = document.querySelector(".feedback");
+//    var popup2 = document.querySelector(".feedback-bg");  
     var close = popup.querySelector(".feedback-close");
-    var map = document.querySelector(".map-img");
+    var bg = document.querySelector(".feedback-bg")
+    
 
-    map.classList.add("map-img-hidden");
     link.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.add("feedback-show");
-        popup2.classList.add("feedback-animation");
+        bg.classList.add("feedback-show");
+        popup.classList.add("feedback-animation");
       });
 
     close.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.remove("feedback-show");
+        bg.classList.remove("feedback-show");
       });
     
-//    popup.addEventListener("click", function(event) {
-//        event.preventDefault();
-//        popup.classList.remove("feedback-show");
-//      });
+    bg.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.remove("feedback-show");
+        bg.classList.remove("feedback-show");
+    })
 
       window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
           if (popup.classList.contains("feedback-show")) {
             popup.classList.remove("feedback-show");
+            bg.classList.remove("feedback-show");
           }
         }
       });
